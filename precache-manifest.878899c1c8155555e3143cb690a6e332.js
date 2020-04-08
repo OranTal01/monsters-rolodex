aa62e82a3d38825eba1c4b21492a69ff3d7ccce2
oran <orantal01@gmail.com>
2020-04-08 18:24:00 +0300

    Updates

---
+++ precache-manifest.878899c1c8155555e3143cb690a6e332.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14d22ecda51d993f658a4c3cfd3871dd",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a0aadda2916c4071750e",
+    "url": "/monsters-rolodex/static/css/main.c691b66c.chunk.css"
+  },
+  {
+    "revision": "ec9bc350c210abfe9443",
+    "url": "/monsters-rolodex/static/js/2.81403498.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.81403498.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0aadda2916c4071750e",
+    "url": "/monsters-rolodex/static/js/main.4d205b69.chunk.js"
+  },
+  {
+    "revision": "83fef09b6b4b91e8e383",
+    "url": "/monsters-rolodex/static/js/runtime-main.6036d2a0.js"
+  }
+]);
